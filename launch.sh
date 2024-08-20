6ad86ab9489fc63b3befc274eba650e36c9cee10
Joel Ye <joelye9@gmail.com>
2024-08-20 15:35:36 -0400

    ready for ndt3 v5 parity

--- launch.sh
+++ launch.sh
@@ -14,5 +14,6 @@ source ~/.bashrc # Note bashrc has been modified to allow slurm jobs
 source ~/load_env.sh
 module unload cudnn-11.6-v8.4.1.50 # not sure where these are coming from but they're failing the rnn runs
 module unload cuda-11.6 # not sure where these are coming from but they're failing the rnn runs
+mamba activate ndt2
 python -u run.py $@
 
